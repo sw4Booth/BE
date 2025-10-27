ae42f5ba2f6c907c9ed067dcd9cbc2da99fe1bc4
jeonseohee9 <seohuij354@gmail.com>
2025-10-27 17:03:56 +0900

    Initial commit

---
+++ src/main/java/com/sw4/Sw4Application.java
@@ -0,0 +1,11 @@
+package com.sw4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sw4Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Sw4Application.class, args);
+    }
+}
